9526378a1de4641adfcca35ad8994c1fb13e00b4
Luiz Fidalgo <luiz.fidalgo@hotmail.com>
2023-09-10 21:34:44 -0300

    Remove uvicorn.run, because of dockerfile

--- src/main.py
+++ src/main.py
@@ -22,6 +22,8 @@ def central_function():
 def prediction_function():
     return {"test": "Prediction"}
 
+"""
 if __name__ == "__main__":
     # Start FastAPI
     uvicorn.run(app, port=8000, host="0.0.0.0")
+"""
