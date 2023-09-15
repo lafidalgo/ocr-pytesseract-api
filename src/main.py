80ef85f29149770ba846c44b192653f7132c40c5
Luiz Fidalgo <luiz.fidalgo@hotmail.com>
2023-09-15 00:45:49 -0300

    Send base64 img & change preprocessing

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ import pytesseract
 import numpy as np
 from io import BytesIO
 import cv2
+import base64
 
 app = FastAPI()
 
@@ -40,6 +41,7 @@ async def submit(params: Params = Depends(), files: List[UploadFile] = File(...)
         # Up-sample
         img_cv2 = cv2.resize(img_cv2, (0, 0), fx=2, fy=2)
 
+        """
         # Apply dilation and erosion to remove some noise
         kernel = np.ones((1, 1), np.uint8)
         img_cv2 = cv2.dilate(img_cv2, kernel, iterations=1)
@@ -47,12 +49,19 @@ async def submit(params: Params = Depends(), files: List[UploadFile] = File(...)
 
         # Apply blur to smooth out the edges
         img_cv2 = cv2.GaussianBlur(img_cv2, (5, 5), 0)
+        """
+        # https://stackoverflow.com/questions/71289347/pytesseract-improving-ocr-accuracy-for-blurred-numbers-on-an-image
+        sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+        img_cv2 = cv2.filter2D(img_cv2, -1, sharpen_kernel)
+        img_cv2 = cv2.threshold(img_cv2, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
+        """
         # Apply threshold to get image with only b&w (binarization)
         img_cv2 = cv2.threshold(img_cv2, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
         # Invert the image
         img_cv2 = cv2.bitwise_not(img_cv2)
+        """
 
         # Down-sample
         img_cv2 = cv2.resize(img_cv2, (0, 0), fx=0.5, fy=0.5)
@@ -64,7 +73,12 @@ async def submit(params: Params = Depends(), files: List[UploadFile] = File(...)
                                         nice=params.nice,
                                         timeout=params.timeout)
         
-        results[file.filename] = ocr
+        _, img_bytes = cv2.imencode('.jpg', img_cv2)
+        final_image_base64 = base64.b64encode(img_bytes).decode('utf-8')
+
+        results[file.filename] = {}
+        results[file.filename]['ocr'] = ocr
+        results[file.filename]['final_image_base64'] = final_image_base64
 
     return {"results": results,
             "params": params}
